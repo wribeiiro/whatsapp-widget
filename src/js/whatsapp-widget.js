91d49d77b95df00147ea745fe89cb80fa06f6997
Wellisson Ribeiro <welleh10@gmail.com>
2020-10-15 13:28:32 -0300

    fix some bugs

--- src/js/whatsapp-widget.js
+++ src/js/whatsapp-widget.js
@@ -19,30 +19,34 @@ let paramsDefault = {
     }
 }
 
+let timer
+
 function initWidget(params)  {
-    const wpp = document.createElement('div')
-    wpp.setAttribute("id", "whatsapp-widget")
-    wpp.classList.add('whatsapp-widget')
 
-    const body = document.querySelector('body')
-    body.appendChild(wpp)
+    document.addEventListener("DOMContentLoaded", function(e) {
 
-    createElements(params)
+        const wpp = document.createElement('div')
+        wpp.setAttribute("id", "whatsapp-widget")
+        wpp.classList.add('whatsapp-widget')
+
+        const body = document.querySelector('body')
+        body.appendChild(wpp)
+
+        createElements(params)
+    })
 }
 
 function createElements(params) {
     const mainWhatsappWidget  = document.querySelector('#whatsapp-widget')
 
-    if (params) {
-        paramsDefault = params;
-    }
+    if (params) 
+        paramsDefault = params
 
     const strTarget = ` <a href="#" id="whatsapp-widget-target" class="whatsapp-widget-target pulse">
                             <img id="whatsapp-widget-icon" class="whatsapp-widget-icon" src="http://200.98.129.191:8080/dashboard/assets/img/whatsapp-icon.png"/>
                         </a>`
 
     const strChat = `<div class="whatsapp-widget-chat" id="whatsapp-widget-chat">
-                    
                         <div class="whatsapp-widget-chat-header" id="whatsapp-widget-chat-header" style="background: ${paramsDefault.optionsPopup.background}; color: ${paramsDefault.optionsPopup.color}">
                             <div class="whatsapp-widget-chat-header close" id="close">x</div>
                             <div class="whatsapp-widget-chat-header bot">
@@ -52,18 +56,16 @@ function createElements(params) {
                                 
                                 <div style="margin-left: 16px; margin-right: 16px">
                                     <div class="whatsapp-widget-chat-header name" id="nameBot">${paramsDefault.optionsBot.name}</div>
-                                    <div class="whatsapp-widget-chat-header status">ONLINE 🟢</div>
+                                    <div class="whatsapp-widget-chat-header status" id="statusBot">ONLINE 🟢</div>
                                 </div>
                             </div>
                         </div>
-
                         <div class="whatsapp-widget-chat-chat" id="whatsapp-widget-chat-chat">
-                            <div class="whatsapp-widget-chat-chat message">
+                            <div class="whatsapp-widget-chat-chat message" id="messageBot">
                                 <div class="whatsapp-widget-chat-chat header" id="userBot"></div>
                                 <div class="whatsapp-widget-chat-chat msg" id="msgBot"></div>
                                 <div class="whatsapp-widget-chat-chat date" id="dateBot"></div>
                             </div>
-
                             <div class="whatsapp-widget-chat-footer" id="whatsapp-widget-chat-footer">
                                 <a href="#" id="whatsapp-widget-open-modal" class="whatsapp-widget-open-modal">   
                                     ${paramsDefault.optionsChat.text}       
@@ -78,46 +80,58 @@ function createElements(params) {
 }
     
 function setEvents() {
+    const close = document.getElementById("close")
+    const modal = document.getElementById("whatsapp-widget-open-modal")
+    const widgetChat = document.getElementById("whatsapp-widget-chat")
+    const widgetTarget = document.getElementById("whatsapp-widget-target")
 
     const setEventClickClose = () => {
-        document.getElementById("close").addEventListener("click", function() {
-            document.getElementById("whatsapp-widget-chat").style.opacity = 0
+        close.addEventListener("click", function() {
+            widgetChat.style.cssText = "visibility: hidden; opacity: 0"
         })
     }
 
     const setEventClickModal = () => {
-        document.getElementById("whatsapp-widget-open-modal").addEventListener("click", function() {
+        modal.addEventListener("click", function(e) {
+            e.preventDefault()
             modalWhatsapp()
+            widgetChat.style.cssText = "visibility: hidden; opacity: 0"
         })
     }
 
     const setEventClickWhatsappIcon = () => {
         const simulateMessage = () => {
+            const messageBot = document.querySelector('#messageBot') 
             const userBot   = document.querySelector('#userBot')
             const msgBot    = document.querySelector('#msgBot')
             const dateBot   = document.querySelector('#dateBot')
+            const statusBot = document.querySelector('#statusBot')
             
+            messageBot.style.display = 'none'
             userBot.innerHTML = ``
-            msgBot.innerHTML  = `${paramsDefault.optionsBot.name} ${paramsDefault.optionsBot.messageTyping}`
+            msgBot.innerHTML = ``
+            statusBot.innerHTML  = paramsDefault.optionsBot.messageTyping
             dateBot.innerHTML = ``
-    
-            setTimeout(() => {
+            
+            clearTimeout(timer)
+
+            timer = setTimeout(() => {
+                messageBot.style.display = 'block'
                 userBot.innerHTML  = paramsDefault.optionsBot.name
                 msgBot.innerHTML   = paramsDefault.optionsBot.messageDefault
+                statusBot.innerHTML = `Online`
                 dateBot.innerHTML  = timeNow()
-            }, 2000);
+            }, 2000)
         }
 
-        document.getElementById("whatsapp-widget-target").addEventListener("click", function(e) {
+        widgetTarget.addEventListener("click", function(e) {
             e.preventDefault()
 
-            const mainWhatsappChat = document.querySelector('#whatsapp-widget-chat')
-
-            if (mainWhatsappChat.style.opacity == 0) {
-                mainWhatsappChat.style.opacity = 1
+            if (window.getComputedStyle(widgetChat).getPropertyValue("opacity") == '0') {
+                widgetChat.style.cssText = "visibility: visible; opacity: 1"
                 simulateMessage()
             } else {
-                mainWhatsappChat.style.opacity = 0
+                widgetChat.style.cssText = "visibility: hidden; opacity: 0"
             }
         })
     }
@@ -131,7 +145,7 @@ function modalWhatsapp() {
     const x = screen.width  / 2 - 800 / 2
     const y = screen.height / 2 - 550 / 2
 
-    window.open(paramsDefault.optionsChat.buttonTarget, ``,'height=550,width=800,left='+x+',top='+y)
+    window.open(paramsDefault.optionsChat.buttonTarget, ``,`height=550,width=800,left=${x},top=${y}`)
 }
 
 function timeNow() {
